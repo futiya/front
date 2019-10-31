17889131205a6f3e85647c5b2c8b7cb6c1fb1441
yuanbing1113 <yuanbing1113@gmail.com>
2019-10-31 20:09:40 +0800

    add index.html

--- server/server.js
+++ server/server.js
@@ -4,6 +4,10 @@ const app = express();
 const port = 8080
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function(req, res) {
+  res.sendFile(path.join(__dirname, '/public', 'index.html'));
+});
+
 app.listen(port, function () {
     console.log('Node.js server started on port: ' + port);
 });
